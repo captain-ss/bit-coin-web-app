304ab7d8709a03c2dfd7171ec8cadea1141883f0
captain-ss <swadeshishu2001@gmail.com>
2022-01-09 18:19:11 +0530

    lets confuse

--- bitcoin-web-app/src/App.js
+++ bitcoin-web-app/src/App.js
@@ -13,28 +13,48 @@ function App() {
         " https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
       )
       .then((res) => {
-        setCoins(res.data);
+        setCoins(res?.data);
         console.log(res.data);
       })
       .catch((error) => console.log(error));
   }, []);
 
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-  };
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
   return (
     <>
       <div className="coin-app">
         <div className="coin-search">
-          <h1 className="coin-text">search a currency</h1>
+          <h1 className="coin-text">Search a currency</h1>
           <form>
             <input
               className="coin-input"
-              placeholder="search"
-              onChange={handleChange()}
+              placeholder="Search"
+              onChange={handleChange}
             />
           </form>
+          {/* {console.log(filteredCoins)} */}
         </div>
+        {filteredCoins.map((coin) => {
+          return ( <>
+             {console.log(coin)}
+            <Coin
+              key={coin?.index}
+              name={coin?.name}
+              image={coin?.image}
+              volume={coin?.market_cap}
+              price={coin?.current_price}
+              symbol={coin?.symbol}
+              priceChange={coin?.price_change_percentage_24h}
+              marketCap={coin?.market_cap}
+            />  </>
+          );
+        })}
+       
       </div>
     </>
   );
